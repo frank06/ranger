f4ea51ff0351f8f8664711348ea8af47509d5b92
frank06 <me@frank06.net>
2015-04-17 20:30:43 -0300

    extension storage wip

--- extension/extension.js
+++ extension/extension.js
@@ -8,7 +8,16 @@ var style = document.createElement('style');
 style.type = 'text/css';
 style.innerHTML = '.ranger-hl { background: rgba(255,255,10,0.5); }';
 document.getElementsByTagName('head')[0].appendChild(style);
-  
+
+// chrome.storage.local.remove(window.location.href);
+
+chrome.storage.local.get(window.location.href, function(data) {
+  if (!data[window.location.href]) return;
+  data[window.location.href].forEach(function(range) {
+    new Ranger(range).paint(hl);
+  })
+});
+
 document.onkeyup = function(e) {
   
   if (e.which == 72) {
@@ -22,10 +31,26 @@ document.onkeyup = function(e) {
       }
       
       var ranger = new Ranger(range);
+      
+      addRange(ranger.toJSON());
+
       ranger.paint(hl);
       window.getSelection().removeAllRanges();
     }
     
   }
+  
+};
+
+var addRange = function(range) {
+  
+  chrome.storage.local.get(window.location.href, function(dictionary) {
+    
+    dictionary[window.location.href] = dictionary[window.location.href] || [];
+    
+    dictionary[window.location.href].push(range);
+    chrome.storage.local.set(dictionary);
+    
+  });
   
 };
